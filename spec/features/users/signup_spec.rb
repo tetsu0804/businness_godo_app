727e94e54f9d2b71655eea152b3558494ae19b99
tetsu <banginhoodyonezawa@gmail.com>
2020-05-25 18:09:44 +0900

    make simple rails_admin and cancan to spec/features/users/signup_spec.rb

--- spec/features/users/signup_spec.rb
+++ spec/features/users/signup_spec.rb
@@ -9,16 +9,21 @@ RSpec.feature 'Signup User', type: :feature do
     login_as @tetsu
 
     visit '/'
-    click_link 'ユーザー登録'
-    # fill_in '性', with: '早坂'
+    click_link '管理者ページ'
+    within first('tbody td') do
+      click_link 'Users'
+    end
+    click_link 'Add new'
+    #fill_in , with: '早坂'
     # fill_in '名', with: '絵理子'
-    fill_in 'メールアドレス', with: 'test2@test.com'
+    fill_in 'Email', with: 'test2@test.com'
     #fill_in '住所', with: '山形県米沢市御廟二丁目'
     fill_in 'Password', with: 'password'
-    fill_in 'Confirmation', with: 'password'
-    click_button 'ユーザー登録'
+    fill_in 'Password confirmation', with: 'password'
+    uncheck 'Admin'
+    click_button 'Save'
     #expect(page).to have_content '新しく早坂 絵理子さんを作成しました'
-    expect(page.current_path).to eq root_path
+    #expect(page.current_path).to eq root_path
     expect(User.count).to eq 2
   end
 end
